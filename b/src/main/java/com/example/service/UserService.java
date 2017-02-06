cad8cfe9f2df0ca545e048442f39ff983867fadb
danidaryaweesh <dani_kurkees@hotmail.com>
2017-02-06 15:54:27 +0100

    right structure with service and implmentation example.

---
+++ b/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.service;
+
+/**
+ * Created by dani on 2017-02-06.
+ */
+
+public interface UserService {
+}
---
+++ b/src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.service.impl;
+
+import com.example.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by dani on 2017-02-06.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+}
