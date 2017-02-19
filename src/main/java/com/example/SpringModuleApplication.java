4c46514e9442de990a230855876eb07cf242cc17
softish <lazar.h.klincov@live.se>
2017-02-19 01:05:55 +0100

    Ditched CORS security for now.

--- src/main/java/com/example/SpringModuleApplication.java
+++ src/main/java/com/example/SpringModuleApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
@@ -19,5 +23,18 @@ public class SpringModuleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringModuleApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+				registry.addMapping("/*/*").allowedOrigins("*");
+				registry.addMapping("/*/*/*").allowedOrigins("*");
+				registry.addMapping("/*/*/*/*").allowedOrigins("*");
+			}
+		};
 	}
 }
